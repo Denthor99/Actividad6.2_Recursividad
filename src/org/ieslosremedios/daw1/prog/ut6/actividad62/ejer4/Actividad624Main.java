1361774f65bf9e8f6c2895730e0bb2fe6d736456
Den <drodsan2708@g.educaand.es>
2023-03-16 14:59:57 +0100

    drodsan2708 - Mejoras en el código - 16/03/2023

--- src/org/ieslosremedios/daw1/prog/ut6/actividad62/ejer4/Actividad624Main.java
+++ src/org/ieslosremedios/daw1/prog/ut6/actividad62/ejer4/Actividad624Main.java
@@ -2,19 +2,14 @@ package org.ieslosremedios.daw1.prog.ut6.actividad62.ejer4;
 
 public class Actividad624Main {
     public static void main(String[] args) {
-    int x=10;
-    int y=5;
+    int x=6;
+    int y=8;
         System.out.println(euclidea(x,y));
     }
     public static int euclidea(int x, int y){
-        if (y>0){
-            return y;
-        }else if (y>x){
-            int x2 =x;
-            int y2 =y;
-            x=y2;
-            y=x2;
-        } else
+        if (y==0){
+            return 0;
+        }else
             return euclidea(y,x%y);
     }
 }
